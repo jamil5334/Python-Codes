03c858fb5c0edd917c10a4894cdea50158c9baa6
jamil5334 <116949098+jamil5334@users.noreply.github.com>
2023-01-16 16:00:04 +0300

    Add files via upload

---
+++ aj.py
@@ -0,0 +1,17 @@
+#program to write HW
+print("Hello World")
+#boolean
+name="Java"
+print(True)
+
+#floating point
+a=56.4
+print(a)
+
+"""proram to print string"""
+name="Python"
+print(name) 
+
+'''program to write integer'''
+b=20
+print(b) 

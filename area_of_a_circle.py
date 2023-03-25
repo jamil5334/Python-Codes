d6fe09e795ed0baeab2985e310c79c007155fb38
jamil5334 <116949098+jamil5334@users.noreply.github.com>
2023-03-25 15:24:56 +0300

    Add files via upload

---
+++ area_of_a_circle.py
@@ -0,0 +1,6 @@
+#function to calculate area of a circle
+def area_of_circle(pi,radius):
+	area=pi*radius*radius
+	print("Area of a circle is:",area)
+
+area_of_circle(3.142,7)
---
+++ employee.py
@@ -0,0 +1,28 @@
+#python code to print employee details and compute overtime
+class Employee(object):
+	"""docstring for Employee"""
+	def __init__(self, emp_id, emp_name, emp_salary, emp_department):
+		self.emp_id = emp_id
+		self.emp_name= emp_name
+		self.emp_salary= emp_salary
+		self.emp_department= emp_department
+
+	def employee_details(self):
+		print("Employee ID:", self.emp_id)
+		print("Employee Name:", self.emp_name)
+		print("Employee Salary:", self.emp_salary)
+		print("Employee Department:", self.emp_department)
+
+	def calculate_emp_salary(self, hours_worked):
+		if hours_worked>50:
+			overtime_hours= hours_worked-50
+			overtime_amount= overtime_hours*(self.emp_salary/50)
+			total_pay= self.emp_salary+overtime_amount
+			return total_pay
+		else:
+			return self.emp_salary
+
+emp= Employee("KU27", "Joy", 65000, "Tourism")
+sal=employee.calculate_emp_salary(60)
+print("Employee's total pay:", salary)
+employee.employee_details()
